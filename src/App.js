2d876953e2f85b580c9c16de6977cddc416584b3
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-28 00:34:58 +0100

    Add the calculator component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator';
 
 function App() {
-
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
