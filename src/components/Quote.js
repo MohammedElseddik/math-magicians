f3b8c3ba65627d9e9a3960ea896f560cebbba1c8
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-06 19:46:15 +0100

    Add Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import './Quote.css';
+
+const Quote = () => (
+  <section className="quote">
+    <p>
+      Mathmatic is not about numbers, equations, computations, or algorithms: it
+      is about understanding. - William Paul Thurston
+    </p>
+  </section>
+);
+
+export default Quote;
