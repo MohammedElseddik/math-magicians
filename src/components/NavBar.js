16d8b9dba09d1bb4348b87d534659b234b66a1cb
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-06 20:11:57 +0100

    Add styles for the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => (
   <nav className="navbar">
-    <span>Math Magicians</span>
-    <Link to="/home">Home</Link>
-    <Link to="/calculator">Calculator</Link>
-    <Link to="/quote">Quote</Link>
+    <span className="title">Math Magicians</span>
+    <div className="links">
+      <Link to="/home">Home</Link>
+      <Link to="/calculator">Calculator</Link>
+      <Link to="/quote">Quote</Link>
+    </div>
   </nav>
 );
 
